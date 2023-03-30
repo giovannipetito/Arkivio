d9645513e18209f73d9f1cac86b46b4c5b71d3f8
giovanni.petito <giovanni.petito@finconsgroup.com>
2023-03-30 13:19:57 +0200

    add config file

---
+++ scripts/config.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+export JAVA_HOME='/usr/lib/jvm/java-11-openjdk-11.0.7.10-4.el7_8.x86_64'
+export JAVA_TOOL_OPTIONS="-Djava.io.tmpdir=$HOME/tmp -Dhttp.proxyHost=proxy.it.bwns.ch -Dhttp.proxyPort=8080 -Dhttps.proxyHost=proxy.it.bwns.ch -Dhttps.proxyPort=8080 -Dhttp.nonProxyHosts='localhost|clienttools.dev.sctv.ch'"
+
+export ANDROID_SDK_ROOT=$HOME/android-sdk/
