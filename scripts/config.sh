4db97a377051098c37fa745d51faffb7bc1de6ea
giovanni.petito <giovanni.petito@finconsgroup.com>
2023-04-05 02:09:32 +0200

    add config file

---
+++ scripts/config.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# export GIT_AUTHOR_EMAIL="nonexistent_wingo_ci@swisscom.com"
+# export GIT_AUTHOR_NAME="Wingo Build"
+# export GIT_COMMITTER_EMAIL="nonexistent_wingo_ci@swisscom.com"
+# export GIT_COMMITTER_NAME="Wingo Build"
+
+export REPOSITORY_URL="git@gitlab.com:giovanni.petito/arkivio.git"
+
+# export http_proxy='http://proxy.it.bwns.ch:8080'
+# export https_proxy='https://proxy.it.bwns.ch:8080'
+# export no_proxy='clienttools.dev.sctv.ch,localhost'
