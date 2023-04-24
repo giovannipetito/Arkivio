20a35541ff5b5d4864771a95952f4b98895c58b9
Giovanni <giovanni.petito88@gmail.com>
2023-04-24 04:42:04 +0200

    remove toolbar

--- app/src/main/java/it/giovanni/arkivio/activities/NavigationActivity.kt
+++ app/src/main/java/it/giovanni/arkivio/activities/NavigationActivity.kt
@@ -1,15 +1,10 @@
 package it.giovanni.arkivio.activities
 
-import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.navigation.NavigationView
 import it.giovanni.arkivio.R
@@ -18,10 +13,8 @@ import it.giovanni.arkivio.fragments.detail.navigation.model.User
 class NavigationActivity : AppCompatActivity() {
 
     private lateinit var navigationView: NavigationView
-
     private lateinit var navController: NavController
     private lateinit var drawerLayout: DrawerLayout
-    private lateinit var appBarConfiguration: AppBarConfiguration
 
     private lateinit var listener: NavController.OnDestinationChangedListener
 
@@ -30,37 +23,26 @@ class NavigationActivity : AppCompatActivity() {
         setContentView(R.layout.navigation_activity)
 
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        navController = navHostFragment.navController
+        navController = navHostFragment.navController // Inizializzo il navController passandogli l'id del NavHostFragment
 
-        // navController = findNavController(R.id.nav_host_fragment) // Inizializzo il navController passandogli l'id del NavHostFragment
         drawerLayout = findViewById(R.id.navigation_drawer_layout)
         navigationView = findViewById(R.id.navigation_view)
         navigationView.setupWithNavController(navController)
 
-        // Inizializzo appBarConfiguration passandogli due parametri, il primo è navController.graph
-        // che usa navController per passare il navigation graph, il secondo parametro è drawerLayout
-        // che è il root layout di navigation_activity.
-        appBarConfiguration = AppBarConfiguration(navController.graph, drawerLayout)
-
-        setupActionBarWithNavController(navController, appBarConfiguration)
-
         listener = NavController.OnDestinationChangedListener { controller, destination, arguments ->
-            if (destination.id == R.id.firstFragment) {
-                supportActionBar?.setBackgroundDrawable(ColorDrawable(getColor(R.color.red_A700)))
-            } else if (destination.id == R.id.secondFragment) {
-                supportActionBar?.setBackgroundDrawable(ColorDrawable(getColor(R.color.light_green_A700)))
-            } else if (destination.id == R.id.argsFragment) {
-                supportActionBar?.setBackgroundDrawable(ColorDrawable(getColor(R.color.light_green_A700)))
-
-                val user = User("Giovanni", "Petito")
-                arguments?.putParcelable("user", user)
+            when (destination.id) {
+                R.id.firstFragment -> {
+                    // todo
+                }
+                R.id.secondFragment -> {
+                    // todo
+                }
+                R.id.argsFragment -> {
+                    val user = User("Giovanni", "Petito")
+                    arguments?.putParcelable("user", user)
+                }
             }
         }
-    }
-
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment)
-        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 
     override fun onResume() {
--- app/src/main/java/it/giovanni/arkivio/fragments/detail/navigation/FirstFragment.kt
+++ app/src/main/java/it/giovanni/arkivio/fragments/detail/navigation/FirstFragment.kt
@@ -4,7 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.Navigation
 import it.giovanni.arkivio.R
 import it.giovanni.arkivio.databinding.FragmentFirstBinding
@@ -31,3 +35,83 @@ class FirstFragment : Fragment() {
         }
     }
 }
+
+/**
+ * La classe Model è dove definirai i dati con cui lavorerà la tua app. Ad esempio, se stai creando
+ * un'app del meteo, il tuo model potrebbe avere campi come temperature, humidity e windSpeed.
+ */
+// data class Weather(val temperature: Double, val humidity: Double, val windSpeed: Double)
+
+/**
+ * La classe ViewModel è la classe dove definirai la logica che la tua app utilizzerà per interagire
+ * con il tuo model. Ad esempio, se desideri recuperare i dati meteorologici da un'API, puoi definire
+ * nel tuo ViewModel la funzione fetchWeatherData() che utilizza Retrofit e Coroutines.
+ *
+ * In questo esempio, weatherService è un servizio Retrofit che è stato definito per effettuare richieste
+ * di rete (network request) a un'API che restituisce i dati meteorologici per una determinata città.
+ *
+ * Il metodo getWeatherData() è una funzione suspend che utilizza Coroutines per effettuare la request
+ * e restituisce un oggetto Response<WeatherData>.
+ *
+ * Il metodo fetchWeatherData() è una funzione suspend che utilizza Coroutines per effettuare la request
+ * e restituisce un oggetto Result<WeatherData>. La classe Result è una classe personalizzata che può
+ * contenere un risultato di successo (Result.Success) o un risultato di errore (Result.Error).
+ *
+ * Nell'implementazione, per prima cosa effettuiamo la request utilizzando il metodo getWeatherData()
+ * e controlliamo se la response ha avuto successo. In caso affermativo, restituiamo un oggetto
+ * Result.Success con i dati meteorologici. Se invece, durante la request viene rilevata un'eccezione,
+ * restituiamo un oggetto Result.Error con l'eccezione rilevata (con il messaggio di eccezione).
+ */
+/*
+class WeatherViewModel : ViewModel() {
+
+    private val weatherData = MutableLiveData<Weather>()
+
+    suspend fun fetchWeatherData(city: String): Result<WeatherData> {
+        // 1. Make API call to fetch weather data
+        // 2. Update weatherData LiveData with the fetched data
+        return try {
+            val response = weatherService.getWeatherData(city)
+            if (response.isSuccessful) {
+                Result.Success(response.body()!!)
+            } else {
+                Result.Error(Exception(response.message()))
+            }
+        } catch (e: Exception) {
+            Result.Error(e)
+        }
+    }
+}
+*/
+/**
+ * La classe View è dove definirai l'interfaccia utente che la tua app mostrerà all'utente.
+ * Ad esempio, potresti avere un file di layout che include un TextView per visualizzare la
+ * temperatura, un ProgressBar per visualizzare l'umidità e così via. Nella tua activity o
+ * fragment, legherai poi questi elementi dell'interfaccia utente al ViewModel.
+ *
+ * Il ViewModel osserva un oggetto LiveData chiamato weatherData, che viene aggiornato quando viene
+ * chiamato fetchWeatherData(). La view quindi osserva l'oggetto weatherData e aggiorna la UI quando
+ * cambia. Ciò consente di separare il codice dell'interfaccia utente dalla business logic e
+ * semplifica il test e la manutenzione del codice.
+ */
+/*
+class WeatherActivity : AppCompatActivity() {
+
+    private lateinit var viewModel: WeatherViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_weather)
+
+        viewModel = ViewModelProvider(this).get(WeatherViewModel::class.java)
+
+        viewModel.weatherData.observe(this, { weather ->
+        // TODO: Update UI with new weather data
+        })
+
+        buttonFetchWeather.setOnClickListener {
+            viewModel.fetchWeatherData()
+        }
+    }
+}
+*/
